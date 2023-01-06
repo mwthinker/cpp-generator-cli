a814810a3997bedcad358d5d5a4dc74c770be3ca
Marcus Welander <mwthinker@yahoo.se>
2023-01-06 21:41:08 +0100

    Clean up

--- src/main/java/se/mwthinker/RepoCommitContent.java
+++ src/main/java/se/mwthinker/RepoCommitContent.java
@@ -3,11 +3,6 @@ package se.mwthinker;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RepoCommitContent {
     @JsonProperty("sha")
@@ -17,34 +12,4 @@ public class RepoCommitContent {
         return sha;
     }
 
-
-    /*
-    public Optional<String> getFirstCommitSha() {
-        return commits.stream()
-                .findFirst()
-                .map(commit -> Optional.ofNullable(commit.sha))
-                .flatMap(Function.identity());
-    }
-    */
 }
-
-/*
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class RepoCommitContent {
-    @JsonProperty("sha")
-    private final List<Commit> commits = new ArrayList<>();
-
-    private static class Commit {
-        @JsonProperty("sha")
-        String sha;
-    }
-
-    public Optional<String> getFirstCommitSha() {
-        return commits.stream()
-                .findFirst()
-                .map(commit -> Optional.ofNullable(commit.sha))
-                .flatMap(Function.identity());
-    }
-}
-
- */
